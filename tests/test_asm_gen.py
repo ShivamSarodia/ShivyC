0b26beab3f33b23b1118685ac4f45bb4c87467cf
Shivam Sarodia <ssarodia@gmail.com>
2016-08-14 01:46:03 -0700

    Add test for adding literal to self

--- tests/test_asm_gen.py
+++ tests/test_asm_gen.py
@@ -164,3 +164,20 @@ class ASTGenTests(unittest.TestCase):
                         "     ret"]
 
         self.assertEqual(asm_code.full_code(), '\n'.join(expected_asm))
+
+    def test_add_literal_to_itself(self):
+        """Test add literal to itself."""
+        il_literal_1 = LiteralILValue(ctypes.integer, "15")
+        il_literal_2 = LiteralILValue(ctypes.integer, "15")
+        il_temp_1 = TempILValue(ctypes.integer)
+        il_code = ILCode()
+        il_code.add_command(ILCode.ADD, il_literal_1, il_literal_2, il_temp_1)
+        il_code.add_command(ILCode.RETURN, il_temp_1)
+
+        asm_code = ASMCode()
+        ASMGen(il_code, asm_code).make_asm()
+
+        expected_asm = ["global main", "", "main:", "     mov eax, 30",
+                        "     ret"]
+
+        self.assertEqual(asm_code.full_code(), '\n'.join(expected_asm))
