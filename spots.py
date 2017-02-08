d4f03e8c55878419654c80bf8013ef293227be20
Shivam Sarodia <ssarodia@gmail.com>
2017-02-08 15:26:49 -0500

    Remove unused code

--- spots.py
+++ spots.py
@@ -49,49 +49,17 @@ class Spot:
         if self.spot_type == self.REGISTER and size == 4:
             return "e" + self.detail[1] + self.detail[2]
         elif self.spot_type == self.STACK and size == 4:
-            if self.detail > 0:
-                return "DWORD [rbp+{}]".format(str(abs(self.detail)))
-            elif self.detail == 0:
-                return "DWORD [rbp]"
-            else:
-                return "DWORD [rbp-{}]".format(str(abs(self.detail)))
+            return "DWORD [rbp-{}]".format(str(abs(self.detail)))
         elif self.spot_type == self.LITERAL:
             return self.detail
         else:
             raise NotImplementedError("Unsupported spot_type/size combo")
 
-    @staticmethod
-    def better(spot1, spot2):
-        """Check if self is preferred over the other spot.
-
-        Specifically, we prefer a register spot over a literal spot over a
-        stack spot.
-
-        """
-        if not spot2:
-            return spot1
-        if not spot1:
-            return spot2
-        elif spot1.spot_type == Spot.REGISTER:
-            return spot1
-        elif spot2.spot_type == Spot.REGISTER:
-            return spot2
-        elif spot1.spot_type == Spot.LITERAL:
-            return spot1
-        elif spot2.spot_type == Spot.LITERAL:
-            return spot2
-        else:  # both are stack
-            return spot1
-
-    # It's very important spots to compare equal iff they have the same type
-    # and detail, so the ASM generation step can keep track of their values as
-    # one unit.
     def __eq__(self, other):
         """Test equality by comparing type and detail."""
         return (self.spot_type, self.detail) == (other.spot_type, other.detail)
 
     def __hash__(self):
-        """Hash based on type and detail."""
         return hash((self.spot_type, self.detail))
 
 RAX = Spot(Spot.REGISTER, "rax")
