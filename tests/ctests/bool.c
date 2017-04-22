909144867aabe9efd3c2fad371e489e9ba275fe9
Shivam Sarodia <ssarodia@gmail.com>
2017-04-22 04:20:10 -0400

    Add boolean test file

---
+++ tests/ctests/bool.c
@@ -0,0 +1,26 @@
+int main() {
+  if((3 && 4) != 1) return 1;
+  if((0 && 4) != 0) return 2;
+  if(0 && 4) return 3;
+  if((3 && 0) != 0) return 4;
+  if(3 && 0) return 5;
+
+  int a, *p = &a;
+  if((p && 0) != 0) return 6;
+  if(p && 0) return 7;
+  if((p && p) != 1) return 8;
+
+  if((3 || 4) != 1) return 9;
+  if((0 || 4) != 1) return 10;
+  if((2 || 0) != 1) return 11;
+  if((0 || 0) != 0) return 12;
+  if(0 || 0) return 13;
+
+  if((0 || 0) != 0) return 12;
+  if((p || 0) != 1) return 14;
+  if((p || p) != 1) return 15;
+
+  if(!p != 0) return 16;
+  if(!p) return 16;
+  if(!0 != 1) return 17;
+}
