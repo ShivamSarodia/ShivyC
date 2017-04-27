517bb6d3e05042c859c735bd8e0720e618cc54ca
Shivam Sarodia <ssarodia@gmail.com>
2017-04-27 17:26:16 -0400

    Improve linking

--- shivyc.py
+++ shivyc.py
@@ -6,6 +6,7 @@ For usage, run "./shivyc.py --help".
 """
 
 import argparse
+import pathlib
 import subprocess
 import sys
 
@@ -150,20 +151,84 @@ def assemble_and_link(binary_name, asm_name, obj_name):
     try:
         subprocess.check_call(["as", "-64", "-o", obj_name, asm_name])
     except subprocess.CalledProcessError:
-        error_collector.add(
-            CompilerError("assembler returned non-zero status"))
+        err = "assembler returned non-zero status"
+        error_collector.add(CompilerError(err))
+        return
+
+    try:
+        crtnum = find_crtnum()
+        if not crtnum: return
+
+        crti = find_library_or_err("crti.o")
+        if not crti: return
+
+        linux_so = find_library_or_err("ld-linux-x86-64.so.2")
+        if not linux_so: return
+
+        crtn = find_library_or_err("crtn.o")
+        if not crtn: return
+
+        # find files to link
+        subprocess.check_call(
+            ["ld", "-dynamic-linker", linux_so, crtnum, crti, "-lc",
+             obj_name, crtn, "-o", binary_name])
+
+    except subprocess.CalledProcessError:
+        err = "linker returned non-zero status"
+        error_collector.add(CompilerError(err))
+
+
+def find_crtnum():
+    """Search for the crt0, crt1, or crt2.o files on the system.
+
+    If one is found, return its path. Else, add an error to the
+    error_collector and return None.
+    """
+    for file in ["crt2.o", "crt1.o", "crt0.o"]:
+        crt = find_library(file)
+        if crt: return crt
+
+    err = "could not find crt0.o, crt1.o, or crt2.o for linking"
+    error_collector.add(CompilerError(err))
+    return None
+
+
+def find_library_or_err(file):
+    """Search the given library file and return path if found.
+
+    If not found, add an error to the error collector and return None.
+    """
+    path = find_library(file)
+    if not path:
+        err = "could not find {}".format(file)
+        error_collector.add(CompilerError(err))
+        return None
     else:
-        try:
-            subprocess.check_call([
-                "ld", "-dynamic-linker", "/lib64/ld-linux-x86-64.so.2",
-                "/usr/lib/x86_64-linux-gnu/crt1.o",
-                "/usr/lib/x86_64-linux-gnu/crti.o",
-                "-lc", obj_name, "/usr/lib/x86_64-linux-gnu/crtn.o", "-o",
-                binary_name
-            ])
-        except subprocess.CalledProcessError:
-            error_collector.add(
-                CompilerError("linker returned non-zero status"))
+        return path
+
+
+def find_library(file):
+    """Search the given library file by searching in common directories.
+
+    If found, returns the path. Otherwise, returns None.
+    """
+    search_paths = [pathlib.Path("/usr/local/lib/x86_64-linux-gnu"),
+                    pathlib.Path("/lib/x86_64-linux-gnu"),
+                    pathlib.Path("/usr/lib/x86_64-linux-gnu"),
+                    pathlib.Path("/usr/local/lib64"),
+                    pathlib.Path("/lib64"),
+                    pathlib.Path("/usr/lib64"),
+                    pathlib.Path("/usr/local/lib"),
+                    pathlib.Path("/lib"),
+                    pathlib.Path("/usr/lib"),
+                    pathlib.Path("/usr/x86_64-linux-gnu/lib64"),
+                    pathlib.Path("/usr/x86_64-linux-gnu/lib")]
+
+    for path in search_paths:
+        full = path.joinpath(file)
+        if full.exists():
+            return str(full)
+    return None
 
 
 if __name__ == "__main__":
