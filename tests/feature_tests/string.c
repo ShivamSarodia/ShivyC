b420e5ed0a81c1558a0546c812bbd44a125b9779
Christian Stephen <chrisj.stephen2@gmail.com>
2018-04-05 11:53:14 +0800

    Add tests for octal/hex escape sequences

--- tests/feature_tests/string.c
+++ tests/feature_tests/string.c
@@ -14,5 +14,21 @@ int main() {
   if('\\' != 92) return 8;
   if(' ' != 32) return 9;
 
+  if('\0' != 0) return 10;
+  if('\40' != 32) return 11;
+  if('\100' != 64) return 12;
+  if('\x9' != 9) return 13;
+  if('\x67' != 103) return 14;
+  if('\x7A' != 122) return 15;
+  if('\x5a' != 90) return 16;
+  if('\x00000021' != 33) return 17;
+
+  if(strcmp("\x68\145\x6c\154\x6F", "hello")) return 18;
+  if(strcmp("12\63", "123")) return 19;
+  if(strcmp("\06123", "123")) return 20;
+  if(strcmp("\578", "/8")) return 21;
+  if(strcmp("\x2fg", "/g")) return 22;
+  if(strcmp("\x", "\x")) return 23;
+
   return 0;
 }
