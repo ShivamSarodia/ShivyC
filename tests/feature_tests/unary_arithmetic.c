22d4cbc003977aafd25ed5c3c84195438ca22b6c
Christian Stephen <chrisj.stephen2@gmail.com>
2018-04-13 15:45:33 +0800

    Add integer promotion test

--- tests/feature_tests/unary_arithmetic.c
+++ tests/feature_tests/unary_arithmetic.c
@@ -1,14 +1,21 @@
 int main() {
   int n = 5;
+  unsigned char c = ' ';
+
+  // test unary minus
   if(-n != 0-5) return 1;
   if(0-n != -5) return 2;
   if(-(n+2) != -7) return 3;
   if(-(-n) != 5) return 4;
 
+  // test unary plus
   if(+n != 5) return 5;
   if(n != +5) return 6;
   if(+(n-2) != 3) return 7;
   if(+(-n) != -5) return 8;
+
+  // test type promotion
+  if(-c != -32) return 9;
 
   return 0;
 }
